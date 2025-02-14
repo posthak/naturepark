28688ca52096dae7471ad8113f14aa1a583dab23
posthak <adler_m@mail.ru>
2025-02-14 09:38:49 +0100

    Website Naturepark init commit

---
+++ home/views.py
@@ -0,0 +1,24 @@
+from django.shortcuts import render, HttpResponse
+
+
+# Create your views here.
+def home(request):
+    # return HttpResponse("This is my Homepage (/)")
+    context = {'name': 'Michael', 'course': 'Django'}
+    return render(request, 'home.html', context)
+
+
+def about(request):
+    # return HttpResponse("This is my about (/about)")
+    context = {'name': 'Michael', 'course': 'Django'}
+    return render(request, 'about.html', context)
+
+
+def projects(request):
+    # return HttpResponse("This is my projects (/projects)")
+    return render(request, 'projects.html')
+
+
+def contact(request):
+    # return HttpResponse("This is my contact (/contact)")
+    return render(request, 'contact.html')
